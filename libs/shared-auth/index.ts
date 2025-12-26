eae173c49bfd9722f408590b7aeef709793792bf
kernelao <oubalkass.ayoub@hotmail.com>
2025-12-26 15:48:45 -0500

    refactor(shared-auth): extract shared jwt verifier, service-specific guard

--- libs/shared-auth/index.ts
+++ libs/shared-auth/index.ts
@@ -2,5 +2,4 @@ export * from './jwt/JwtVerifier.port';
 export * from './jwt/JoseJwtVerifier';
 export * from './jwt/pem';
 
-export * from './nest/JwtAuth.guard';
 export * from './nest/jwt.providers';
